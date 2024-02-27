c021a6fdff20731ad3bcd0b32a9815549cdca1bd
Sumedh Nagpure <sumedhnagpure123@gmail.com>
2024-02-27 10:39:25 +0530

    Like & Dislike redirect from Dashboard to Info

--- client/src/components/Dislikes.js
+++ client/src/components/Dislikes.js
@@ -1,17 +1,16 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 import swal from "sweetalert";
-export default function Dislikes({
-  accountinformation,
-  getAccountInformation,
-}) {
-  // useEffect(() => {
-  //   getAccountInformation();
-  // }, []);
-  let totalitems = accountinformation.dislikes.length;
+import { useNavigate } from "react-router-dom";
+
+export default function Dislikes({ accountinformation, getAccountInformation }) {
+  let navigate = useNavigate();
+
+  async function goToMovie(id) {
+    navigate(`/movie/${id}`);
+  }
 
   async function updateDislikes(movie) {
-   
     let token = localStorage.getItem("moviesToken");
 
     let response = await axios.post(
@@ -23,37 +22,40 @@ export default function Dislikes({
         },
       }
     );
-    if (response) {
-     
-      if (response.status === 200) {
-      
-        swal("Deleted ", response.data.message, "success").then((response) => {
-          getAccountInformation();
-        });
-      }
+    
+    if (response && response.status === 200) {
+      swal("Deleted ", response.data.message, "success").then(() => {
+        getAccountInformation();
+      });
     }
   }
+
   return (
-    <div className="  px-3 my-2 lg:my-0 lg:px-10 h-screen">
-      <span className="block text-start text-3xl ">Dislikes </span>
-      <div className="grid grid-cols-2 lg:grid-cols-4 my-8 ">
+    <div className="px-3 my-2 lg:my-0 lg:px-10 h-screen">
+      <span className="block text-start text-3xl">Dislikes </span>
+      <div className="grid grid-cols-2 lg:grid-cols-4 my-8">
         {accountinformation.dislikes && accountinformation.dislikes.length > 0 ? (
           accountinformation.dislikes.map((like, index) => {
             return (
               <div
-                className={`border bg-center bg-no-repeat bg-cover  h-72 bg-[url(https://image.tmdb.org/t/p/original${
+                onClick={() => {
+                  goToMovie(like.originalId);
+                }}
+                className={`border bg-center bg-no-repeat bg-cover h-72 bg-[url(https://image.tmdb.org/t/p/original${
                   like && like.movieBackdropPath
-                })]  `}
+                })]`}
+                key={like.originalId} // Adding key prop for React list rendering
               >
-                <div className="flex  justify-between  mt-1 font-semibold text-white ">
-                  <span className="text-xs px-2 py-1 rounded-full hover:text-black hover:bg-gray-300 duration-200 cursor-pointer ">
+                <div className="flex justify-between mt-1 font-semibold text-white">
+                  <span className="text-xs px-2 py-1 rounded-full hover:text-black hover:bg-gray-300 duration-200 cursor-pointer">
                     {like.movieName}
                   </span>
                   <span
-                    onClick={() => {
+                    onClick={(e) => {
+                      e.stopPropagation(); // Prevent the click from bubbling to the outer div
                       updateDislikes(like);
                     }}
-                    className="text-red-500 text-2xl hover:text-3xl duration-100  mr-1 mt-1 cursor-pointer "
+                    className="text-red-500 text-2xl hover:text-3xl duration-100 mr-1 mt-1 cursor-pointer"
                   >
                     <ion-icon name="close-circle-outline"></ion-icon>
                   </span>
@@ -62,7 +64,7 @@ export default function Dislikes({
             );
           })
         ) : (
-          <span className="text-start block  my-8 text-gray-200">
+          <span className="text-start block my-8 text-gray-200">
             Nothing in the dislikes
           </span>
         )}
--- client/src/components/Likes.js
+++ client/src/components/Likes.js
@@ -1,14 +1,16 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 import swal from "sweetalert";
+import { useNavigate } from "react-router-dom";
+
 export default function Likes({ accountinformation, getAccountInformation }) {
-  // useEffect(() => {
-  //   getAccountInformation();
-  // }, []);
-  let totalitems = accountinformation.likes.length;
+  let navigate = useNavigate();
+
+  async function goToMovie(id) {
+    navigate(`/movie/${id}`);
+  }
 
   async function updateLikes(movie) {
- 
     let token = localStorage.getItem("moviesToken");
 
     let response = await axios.post(
@@ -20,37 +22,39 @@ export default function Likes({ accountinformation, getAccountInformation }) {
         },
       }
     );
-    if (response) {
-      
-      if (response.status === 200) {
-     
-        swal("Deleted ", response.data.message, "success").then((response) => {
-          getAccountInformation();
-        });
-      }
+    
+    if (response && response.status === 200) {
+      swal("Deleted ", response.data.message, "success").then((response) => {
+        getAccountInformation();
+      });
     }
   }
+
   return (
-    <div className="  px-3 my-2 lg:my-0 lg:px-10 h-screen">
-      <span className="block text-start text-3xl ">Likes </span>
-      <div className="grid grid-cols-2 lg:grid-cols-4 my-8 ">
+    <div className="px-3 my-2 lg:my-0 lg:px-10 h-screen">
+      <span className="block text-start text-3xl">Likes </span>
+      <div className="grid grid-cols-2 lg:grid-cols-4 my-8">
         {accountinformation.likes && accountinformation.likes.length > 0 ? (
           accountinformation.likes.map((like, index) => {
             return (
               <div
-                className={`border bg-center bg-no-repeat bg-cover  h-72 bg-[url(https://image.tmdb.org/t/p/original${
+                onClick={() => {
+                  goToMovie(like.originalId);
+                }}
+                className={`border bg-center bg-no-repeat bg-cover h-72 bg-[url(https://image.tmdb.org/t/p/original${
                   like && like.movieBackdropPath
-                })]  `}
+                })]`}
               >
-                <div className="flex  justify-between  mt-1 font-semibold text-white ">
-                  <span className="text-xs px-2 py-1 rounded-full hover:text-black hover:bg-gray-300 duration-200 cursor-pointer ">
+                <div className="flex justify-between mt-1 font-semibold text-white">
+                  <span className="text-xs px-2 py-1 rounded-full hover:text-black hover:bg-gray-300 duration-200 cursor-pointer">
                     {like.movieName}
                   </span>
                   <span
-                    onClick={() => {
+                    onClick={(e) => {
+                      e.stopPropagation(); // Prevent the click from bubbling to the outer div
                       updateLikes(like);
                     }}
-                    className="text-red-500 text-2xl hover:text-3xl duration-100  mr-1 mt-1 cursor-pointer "
+                    className="text-red-500 text-2xl hover:text-3xl duration-100 mr-1 mt-1 cursor-pointer"
                   >
                     <ion-icon name="close-circle-outline"></ion-icon>
                   </span>
@@ -59,7 +63,7 @@ export default function Likes({ accountinformation, getAccountInformation }) {
             );
           })
         ) : (
-          <span className="text-start block  my-8 text-gray-200">
+          <span className="text-start block my-8 text-gray-200">
             Nothing in the likes
           </span>
         )}
